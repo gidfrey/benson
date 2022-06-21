bfd74778f75d9c2e5ae335a10d31ace8f6c685b3
gidfrey <104103041+gidfrey@users.noreply.github.com>
2022-06-21 15:23:18 +0300

    Update godfrey.py

--- godfrey.py
+++ godfrey.py
@@ -1,16 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[7]:
 
-
-
-import streamlit as st
-C=st. number_input ("enter your age")
-if C>=18:
-        st.write("eligibility")
-elif C<=18:
-        st.write("no eligibility")
 
 
 # In[1]:
