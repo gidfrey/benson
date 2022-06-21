c8511a41e917698453514f6107805da82e0412cb
gidfrey <104103041+gidfrey@users.noreply.github.com>
2022-06-21 15:15:27 +0300

    Add files via upload

---
+++ godfrey.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+
+import streamlit as st
+C=st. number_input ("enter your age")
+if C>=18:
+        st.write("eligibility")
+elif C<=18:
+        st.write("no eligibility")
+
+
+# In[1]:
+
+
+
+import streamlit as st
+st.header("vote program")
+C=st. number_input ("enter your age")
+if C>=18:
+        st.write("eligibility")
+elif C<=18:
+        st.write("no eligibility")
+        
+
+
+# In[ ]:
+
+
+
+
